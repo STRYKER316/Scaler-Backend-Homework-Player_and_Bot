d02a0acdb53337aba7757d184e05b2eb43ee1e19
Stryker316 <ansil.mishra316@gmail.com>
2023-09-14 00:12:29 +0530

    Palyer model made into a DB entity

--- src/main/java/com/example/playerandbot/models/Player.java
+++ src/main/java/com/example/playerandbot/models/Player.java
@@ -1,6 +1,14 @@
 package com.example.playerandbot.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Player {
+    @Id
     private long id;
     private String name;
     private int age;
