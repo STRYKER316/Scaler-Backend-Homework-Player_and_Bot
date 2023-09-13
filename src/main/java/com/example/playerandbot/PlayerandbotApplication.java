03dc058637f2e319566a011fccb69d7a664e5a72
Stryker316 <ansil.mishra316@gmail.com>
2023-09-13 22:55:37 +0530

    Initial Commit

---
+++ src/main/java/com/example/playerandbot/PlayerandbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.playerandbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerandbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayerandbotApplication.class, args);
+    }
+
+}
